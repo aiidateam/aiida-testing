8929b14dc5fdda275b85ed8e70b16e94fc33aaac
Dominik Gresch <a-dogres@microsoft.com>
2019-12-03 14:45:01 +0100

    Initial commit.

---
+++ aiida_pytest_mock_codes/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""A pytest plugin for mocking AiiDA codes."""
+
+__version__ = '0.0.0a1'
---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Author: Dominik Gresch <greschd@gmx.ch>
+"""
+usage: pip install .[dev]
+"""
+
+import json
+import os
+
+from setuptools import find_packages, setup
+
+SETUP_JSON_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'setup.json')
+with open(SETUP_JSON_PATH, 'r') as json_file:
+    SETUP_KWARGS = json.load(json_file)
+EXTRAS_REQUIRE = SETUP_KWARGS['extras_require']
+EXTRAS_REQUIRE['dev'] = (
+    EXTRAS_REQUIRE["docs"] + EXTRAS_REQUIRE["testing"] + EXTRAS_REQUIRE["dev_precommit"]
+)
+
+if __name__ == '__main__':
+    setup(
+        packages=find_packages(),
+        long_description=open(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+        ).read(),
+        long_description_content_type="text/markdown",
+        **SETUP_KWARGS
+    )
